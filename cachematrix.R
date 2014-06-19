4918798bce4a9955e72a2cd877ea9fc75a47be58
BDGitJS <ben.berlin2@hotmail.com>
2014-06-19 12:49:56 +0100

    ProgAssign2_Cache_Matrix
    Two functions on matrix and cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+#returns a list of functions to set and get matrix and set and get matrix inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+	
+		# m to store the inverse
+        m <- NULL
+        
+        # get and set the matrix
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        get <- function() x
+        
+        # get and set the inverse
+        setmatrix <- function(solve) m <<- solve
+        
+        getmatrix <- function() m
+        
+        # return the matrix
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+# compute the inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getmatrix()
+
+		# find the inverse in the cache and if it's there return it
+        if(!is.null(m)) {
+                message("getting cached imatrix")
+                return(m)
+        }
+        
+        # if no inverse present calculate it
+        matrix <- x$get()
+        m <- solve(matrix, ...)
+        
+        # and cache it
+        x$setmatrix(m)
+        
+        # and return it
+        m
 }
